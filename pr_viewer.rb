75d6df66f8da1b14af69584019509769fca9acc4
Chris Peak <christopeak@gmail.com>
2015-02-24 20:55:20 -0800

    working export to basic html

--- pr_viewer.rb
+++ pr_viewer.rb
@@ -16,47 +16,45 @@ class PrReader
   require 'open-uri'
   require 'nokogiri'
   
-  url = 'C:\Users\Chris\Documents\Ruby\pr_viewer\progress_reports.xml'
-  xml = open(url)
-  xml_doc = Nokogiri::XML(xml)
-  
-  item_nodes = xml_doc.xpath('/progress-reports/progress-report')
-  items = item_nodes.map do |node|
-    node.elements.reduce({}) do |item, el|
-      item[el.name] = el.content.to_s
-      item
+  def initialize(filepath)
+    @url = filepath
+    @xml = open(@url)
+    @xml_doc = Nokogiri::XML(@xml)
+    @item_nodes = @xml_doc.xpath('/progress-reports/progress-report')
+    @items = @item_nodes.map do |node|
+      node.elements.reduce({}) do |item, el|
+        item[el.name] = el.content.to_s
+        item
+      end
+    end
+    @prs = @items.map do|i| 
+      PR.new(i)
     end
   end
-  prs = items.map do|i| 
-    PR.new(i)
-  end
-  #prs.each {|p| puts "Status: #{p.status} -- EnvDate: #{p.environmental_approval_on}"}; nil
   
   def render_pr_record(pr)
   <<-REC
-<TR>
-  <TD>#{pr.tip_id}</TD>
-  <TD>#{pr.title}</TD>
-  <TD>#{pr.progress}</TD>
-</TR>
+  <TR>
+    <TD>#{pr.tip_id}</TD>
+    <TD>#{pr.title}</TD>
+    <TD>#{pr.progress}</TD>
+  </TR>
   REC
   end
   
-  def render_prs(prs)
+  def render_prs()
     <<-PR
-<TABLE><TR><TH>Project ID</TH><TH>Title</TH><TH><Progress></TH></TR>
-  #{prs.map {|pr| render_pr_record(pr)}.join "\n"}
+<TABLE><TR><TH>Project ID</TH><TH>Title</TH><TH>Progress</TH></TR>
+#{@prs.map {|pr| render_pr_record(pr)}.join "\n"}
 </TABLE>   
     PR
   end
-  
-  File.open('pr_out.html','w') do |outfile|
-    outfile.puts "<HTML><BODY>"
-    #outfile.puts render_prs(prs)
-    self.render_pr_record(prs[2])
-    #outfile.puts "hello"
-    outfile.puts "</BODY></HTML>"
-  end
-  nil
-  
+    
+end
+
+r = PrReader.new('C:\Users\Chris\Documents\Ruby\pr_viewer\progress_reports.xml')
+File.open('pr_out.html','w') do |outfile|
+  outfile.puts "<HTML><BODY>"
+  outfile.puts r.render_prs()
+  outfile.puts "</BODY></HTML>"
 end
--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
 class MyClass
-  def method1
+  def self.method1
     puts "hello"
   end
   
